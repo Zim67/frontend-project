fcf694c804b9378553b48dccd3f847db8400cbce
Brady Green <138686566+Brady-G@users.noreply.github.com>
2024-08-09 12:39:42 -0230

    Fix frontend admin flights not working correctly

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -12,15 +12,29 @@ const AdminPage = () => {
         toGate: '',
         arrivingTime: ''
     });
+    const [airports, setAirports] = useState([]);
+    const [aircrafts, setAircrafts] = useState([]);
 
     useEffect(() => {
         fetchFlights();
+        fetchAirports();
+        fetchAircrafts();
     }, []);
 
     const fetchFlights = async () => {
         const response = await axios.get('http://localhost:8080/flights');
         setFlights(response.data);
     };
+
+    const fetchAirports = async () => {
+        const response = await axios.get('http://localhost:8080/airports');
+        setAirports(response.data);
+    }
+
+    const fetchAircrafts = async () => {
+        const response = await axios.get('http://localhost:8080/aircrafts');
+        setAircrafts(response.data);
+    }
 
     const handleChange = (e) => {
         setNewFlight({
@@ -30,12 +44,20 @@ const AdminPage = () => {
     };
 
     const handleCreateFlight = async () => {
-        await axios.post('http://localhost:8080/flights/create', newFlight);
+        await axios.post('http://localhost:8080/flights/create', {
+            aircraft: Number.parseInt(newFlight.aircraftId),
+            to: Number.parseInt(newFlight.toAirportId),
+            from: Number.parseInt(newFlight.fromAirportId),
+            toGate: newFlight.toGate,
+            fromGate: newFlight.fromGate,
+            arrivingTime: newFlight.arrivingTime,
+            leavingTime: newFlight.leavingTime,
+        });
         fetchFlights();
     };
 
     const handleDeleteFlight = async (id) => {
-        await axios.delete('http://localhost:8080/flights/${id}');
+        await axios.delete(`http://localhost:8080/flights/${id}`);
         fetchFlights();
     };
 
@@ -44,13 +66,34 @@ const AdminPage = () => {
             <h1>Admin Page</h1>
             <div className="new-flight-form">
                 <h2>Create New Flight</h2>
-                <input type="text" name="aircraftId" placeholder="Aircraft ID" onChange={handleChange} />
-                <input type="text" name="fromAirportId" placeholder="From Airport ID" onChange={handleChange} />
+                <select name="aircraftId" onChange={handleChange} defaultValue="">
+                    <option value="" disabled>Select an aircraft</option>
+                    {aircrafts.map(aircraft => (
+                        <option key={aircraft.id} value={aircraft.id}>{aircraft.registrationNumber}</option>
+                    ))}
+                </select>
+                <br/>
+                <br/>
+                <select name="fromAirportId" onChange={handleChange} defaultValue="">
+                    <option value="" disabled>Select an leaving airport</option>
+                    {airports.map(airport => (
+                        <option key={airport.id} value={airport.id}>{airport.name}</option>
+                    ))}
+                </select>
                 <input type="text" name="fromGate" placeholder="From Gate" onChange={handleChange} />
-                <input type="text" name="leavingTime" placeholder="Leaving Time" onChange={handleChange} />
-                <input type="text" name="toAirportId" placeholder="To Airport ID" onChange={handleChange} />
+                <input type="datetime-local" name="leavingTime" placeholder="Leaving Time" onChange={handleChange} />
+                <br/>
+                <br/>
+                <select name="toAirportId" onChange={handleChange} defaultValue="">
+                    <option value="" disabled>Select an arriving airport</option>
+                    {airports.map(airport => (
+                        <option key={airport.id} value={airport.id}>{airport.name}</option>
+                    ))}
+                </select>
                 <input type="text" name="toGate" placeholder="To Gate" onChange={handleChange} />
-                <input type="text" name="arrivingTime" placeholder="Arriving Time" onChange={handleChange} />
+                <input type="datetime-local" name="arrivingTime" placeholder="Arriving Time" onChange={handleChange} />
+                <br/>
+                <br/>
                 <button onClick={handleCreateFlight}>Create Flight</button>
             </div>
             <div className="flights-list">
@@ -58,7 +101,8 @@ const AdminPage = () => {
                 <ul>
                     {flights.map(flight => (
                         <li key={flight.id}>
-                            {flight.aircraftId} - {flight.fromAirportId} to {flight.toAirportId}
+                            <span>{flight.aircraft.id} - {flight.from.name} to {flight.to.name}</span>
+                            <span> </span>
                             <button onClick={() => handleDeleteFlight(flight.id)}>Delete</button>
                         </li>
                     ))}
